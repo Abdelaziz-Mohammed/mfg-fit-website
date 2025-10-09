593543d9e7223df5ed0787b96144f4d5f77ab921
Abdelaziz Mohamed <abdelazizmo2022@gmail.com>
2025-10-09 15:50:23 +0300

    Bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,27 @@ import "./utils/i18n.js";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ContactProvider } from "./context/ContactContext.jsx";
 import { AdminProvider } from "./context/AdminContext.jsx";
+import { toast } from "react-toastify";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("user");
+
+      delete axios.defaults.headers.common["Authorization"];
+
+      toast.error("Session expired. Please log in again.");
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <AdminProvider>
